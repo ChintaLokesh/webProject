fdfa05a4ba77a89a8e4b130e4fae0af077b67824
lokesh.chinta <lokeech@gmail.com>
2025-10-17 16:52:03 +0530

    FirstCommit

---
+++ untitled/src/main/java/utils/ReUsableMethods.java
@@ -0,0 +1,39 @@
+package utils;
+
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReUsableMethods {
+
+    WebDriver driver;
+   public  ReUsableMethods(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public void takeScreenshot(String name) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd_MM_yyyy_HH_mm");
+            File file = new File(System.getProperty("user.dir")+"/Screenshots");
+            file.mkdir();
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String fileName=LocalDateTime.now().format(formatter).toString()+name;
+            FileUtils.copyFile(src, new File(System.getProperty("user.dir") + "/Screenshots/" + fileName+".png"));
+        }
+        catch(IOException exception)
+        {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+}
